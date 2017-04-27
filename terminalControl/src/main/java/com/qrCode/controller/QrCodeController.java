32c9dcb382f939588dad3016a2adf4260238d28f
shisihai <htohoot@163.com>
2017-04-27 21:04:21 +0800

    添加Travelertools
    添加Travelertools

--- terminalControl/src/main/java/com/qrCode/controller/QrCodeController.java
+++ terminalControl/src/main/java/com/qrCode/controller/QrCodeController.java
@@ -15,40 +15,39 @@ import com.qrCode.service.QrCodeServiceI;
 @Controller
 @RequestMapping("/qrCode")
 public class QrCodeController extends BaseController {
-	@Autowired
-	QrCodeServiceI qrCodeService;
-	/**
-	 * <p>功能描述：根据内容批量生成二维码图片</p>
-	 *@return
-	 *作者：SShi11
-	 *日期：Apr 18, 2017 11:10:15 AM
-	 */
-	@RequestMapping("/batchGenQrcode")
-	public String batchGenQrcode(String qrCodes,Map<String,Object> rs,HttpServletRequest request,HttpSession session){
-		try {
-			rs.put("qrCodes", qrCodes);
-			rs=qrCodeService.batchGenQrcode(rs);
-		} catch (Exception e) {
-			e.printStackTrace();
-			rs.put(ERRMSG, e.getMessage());
-		}
-		return  "/qrCode/qrCodeIMG.jsp";
-	}
-	
-	/**
-	 * <p>功能描述：跳转到批量生成二维码页面</p>
-	 *@param qrCodes
-	 *@param rs
-	 *@param request
-	 *@param session
-	 *@return
-	 *作者：SShi11
-	 *日期：Apr 24, 2017 5:30:51 PM
-	 */
-	@RequestMapping("/goToGenQRCode")
-	public String goToGenQRCode(HttpServletRequest request,HttpSession session){
-		return  "/qrCode/areaQrCode.jsp";
-	}
-	
-	
+    @Autowired
+    QrCodeServiceI qrCodeService;
+
+    /**
+     * <p>功能描述：根据内容批量生成二维码图片</p>
+     *
+     * @return 作者：SShi11
+     * 日期：Apr 18, 2017 11:10:15 AM
+     */
+    @RequestMapping("/batchGenQrcode")
+    public String batchGenQrcode(String qrCodes, Map<String, Object> rs, HttpServletRequest request, HttpSession session) {
+        try {
+            rs.put("qrCodes", qrCodes);
+            rs = qrCodeService.batchGenQrcode(rs);
+        } catch (Exception e) {
+            e.printStackTrace();
+            rs.put(ERRMSG, e.getMessage());
+        }
+        return "/qrCode/qrCodeIMG.jsp";
+    }
+
+    /**
+     * <p>功能描述：跳转到批量生成二维码页面</p>
+     * *@param request
+     *
+     * @param session
+     * @return 作者：SShi11
+     * 日期：Apr 24, 2017 5:30:51 PM
+     */
+    @RequestMapping("/goToGenQRCode")
+    public String goToGenQRCode(HttpServletRequest request, HttpSession session) {
+        return "/qrCode/areaQrCode.jsp";
+    }
+
+
 }
